1609c29ca5de064c1b900936bc1ef7ad9c65a726
Jean-Pascal Richir <jrichir@student.s19.be>
2024-06-27 12:52:26 +0200

    working base & main.c a la norme

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: jrichir <jrichir@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 15:33:37 by jrichir           #+#    #+#             */
-/*   Updated: 2024/06/27 12:17:58 by jrichir          ###   ########.fr       */
+/*   Updated: 2024/06/27 12:45:40 by jrichir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,15 @@
 # include <mlx.h>
 //# include <unistd.h>
 //# include <stdlib.h>
+
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}			t_data;
 
 //# define EXIT_ERROR_USAGE "Usage : ./client [server_id] [string]\n"
 
--- src/main.c
+++ src/main.c
@@ -6,17 +6,38 @@
 /*   By: jrichir <jrichir@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 10:11:15 by jrichir           #+#    #+#             */
-/*   Updated: 2024/06/27 12:19:28 by jrichir          ###   ########.fr       */
+/*   Updated: 2024/06/27 12:51:08 by jrichir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <so_long.h>
 
-//int	main(int argc, char **argv)
 int	main(int argc, char **argv)
 {
+	t_data	img;
 	void	*mlx;
 
 	mlx = mlx_init();
+	img.img = mlx_new_image(mlx, 360, 360);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+			&img.endian);
 	return (0);
 }
+
+/*
+//int	main(int argc, char **argv)
+int	main(int argc, char **argv)
+{
+	t_data	img;
+	void	*mlx;
+	//void	*mlx_win;
+
+	mlx = mlx_init();
+	img.img = mlx_new_image(mlx, 360, 360);
+	//mlx_win = mlx_new_window(mlx, 600, 600, "My first MLX window");
+	//mlx_loop(mlx);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								 &img.endian);
+	return (0);
+}
+*/
