7dedeba8a71bc029d571d0970e100e0cd2374912
Jean-Pascal Richir <jrichir@student.s19.be>
2024-07-15 15:43:47 +0200

    checks map file extension

--- src/check_path.c
+++ src/check_path.c
@@ -14,11 +14,18 @@
 
 int	is_valid_path(int argc, char **argv)
 {
-	int	fd;
+	int		fd;
+	int		comp;
 
 	if (argc != 2)
 	{
 		ft_putstr_fd("Usage: ./so_long maps/MAP_NAME.ber\n", 2);
+		return (0);
+	}
+	comp = ft_strncmp(".ber", &argv[1][ft_strlen(argv[1]) - 4], 4);
+	if (comp)
+	{
+		ft_putstr_fd("Map must have a \".ber\" file extension.\n", 2);
 		return (0);
 	}
 	fd = open(argv[1], O_RDWR);
